4d169ec1c572bbf32c6741cc297041dcafc42733
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-10-23 00:11:17 +0200

    solved es4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,6 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const autore = posts.map(posts => posts.author)
+console.log(autore);
